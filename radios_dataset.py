b6c48551cb3a45a3312633d9b0370724389a067b
Apinsun <arnaudpinsun@gmail.com>
2026-05-13 17:38:20 +0200

    début de la partie 4 avec train sur les données faiblement labellisée

--- radios_dataset.py
+++ radios_dataset.py
@@ -1,25 +1,41 @@
 from PIL import Image
 from torch.utils.data import Dataset
+from pathlib import Path
 
 class RadiosDataset(Dataset):
-    def __init__(self, liste_chemins, transformations):
-        # On stocke les chemins
-        self.liste_chemins = liste_chemins
+    def __init__(self, chemins_images, labels=None, transformations=None):
+        
+        # --- 1. L'AUTO-DÉTECTION (Fini l'erreur PosixPath !) ---
+        # Si on donne juste un chemin de dossier, la classe cherche les images toute seule
+        if isinstance(chemins_images, (str, Path)) and Path(chemins_images).is_dir():
+            self.chemins = [fichier for fichier in Path(chemins_images).iterdir() if fichier.is_file()]
+        else:
+            # Si c'est déjà une liste d'images, on la garde telle quelle
+            self.chemins = list(chemins_images)
+            
+        self.labels = labels
         self.transformations = transformations
 
+        # --- 2. LE FILET DE SÉCURITÉ ---
+        # On s'assure qu'on n'a pas décalé nos listes (très important en Deep Learning)
+        if self.labels is not None:
+            if len(self.chemins) != len(self.labels):
+                raise ValueError(f"CRASH ÉVITÉ : Vous avez {len(self.chemins)} images mais {len(self.labels)} labels !")
+
     def __len__(self):
-        # Retourne le nombre total d'images
-        return len(self.liste_chemins)
+        return len(self.chemins)
 
     def __getitem__(self, idx):
-        # Cette fonction est appelée automatiquement par le DataLoader
-        # Elle charge l'image numéro 'idx' depuis le disque dur
-        chemin_fichier = self.liste_chemins[idx]
+        chemin = self.chemins[idx]
+        image_pil = Image.open(chemin).convert('RGB')
         
-        # .convert('RGB') est une sécurité au cas où une image serait en niveaux de gris pur
-        image_pil = Image.open(chemin_fichier).convert('RGB') 
-        
-        # On applique ton pipeline ResNet
-        image_tensor = self.transformations(image_pil)
-        
-        return image_tensor
+        if self.transformations:
+            image_tensor = self.transformations(image_pil)
+        else:
+            image_tensor = image_pil
+            
+        if self.labels is not None:
+            label = self.labels[idx]
+            return image_tensor, label
+        else:
+            return image_tensor
